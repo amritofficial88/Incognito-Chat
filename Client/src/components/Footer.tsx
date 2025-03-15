38348c4ae46689cc635821fe6783fccad7aa9805
amritofficial88 <167979593+amritofficial88@users.noreply.github.com>
2025-03-16 03:23:54 +0530

    Add files via upload

---
+++ Client/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="text-center py-6 font-medium px-6 bg-neutral-900 text-neutral-200 text-sm sm:text-base">
+      Created By: <span className="font-semibold">Amritangshu Dey</span>
+    </div>
+  );
+};
+
+export default Footer;
+
